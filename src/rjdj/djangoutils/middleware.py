3897f336e38ecf2402fa694a6a8c05ed00803244
Christian Haudum <christian.haudum@gmail.com>
2011-09-19 11:02:48 +0100

    added MultipleProxyMiddleware

--- src/rjdj/djangoutils/middleware.py
+++ src/rjdj/djangoutils/middleware.py
@@ -22,26 +22,48 @@ class SSLRedirect:
 
     def process_request(self, request):
         pass
-#        if settings.DEBUG:
-#            print "SSL_DEBUG: hostname=%s" % get_host(request)
-#            print "SSL_DEBUG: uri=%s" % request.get_full_path()
-
 
     def process_response(self, request, response):
-#        if settings.DEBUG:
-#            print "SSL_DEBUG: status_code=%s" % response.status_code
-        if (response.status_code == 302 or \
-            response.status_code == 301) and not \
-            response["Location"].startswith("http"):
+        ## print "SSL_DEBUG: hostname=%s" % get_host(request)
+        ## print "SSL_DEBUG: uri=%s" % request.get_full_path()
+        if (response.status_code == 302 or response.status_code == 301) and \
+                not response["Location"].startswith("http"):
+
             if response["Location"].startswith("/"):
+                ## Redirect location is an absolute path starting with a leading slash
                 abs_path = response["Location"]
             else:
+                ## Redirect location is a relative path starting with ./ or ../
                 abs_path = path.abspath(path.join(request.get_full_path(),response["Location"]))
                 if response["Location"].endswith("/"):
+                    ## Add a trailing slash if redirect location had one
                     abs_path += "/"
+
+            ## Generate new URL
             newurl = "https://%s%s" % (get_host(request),abs_path)
-#            if settings.DEBUG:
-#                print "SSL_DEBUG: redirect=%s" % newurl
+            ## print "SSL_DEBUG: redirect=%s" % newurl
             return HttpResponsePermanentRedirect(newurl)
+
         else:
             return response
+
+
+class MultipleProxyMiddleware:
+    """https://docs.djangoproject.com/en/1.3/ref/request-response/"""
+
+    FORWARDED_FOR_FIELDS = [
+        'HTTP_X_FORWARDED_FOR',
+        'HTTP_X_FORWARDED_HOST',
+        'HTTP_X_FORWARDED_SERVER',
+    ]
+
+    def process_request(self, request):
+        """
+        Rewrites the proxy headers so that only the most
+        recent proxy is used.
+        """
+        for field in self.FORWARDED_FOR_FIELDS:
+            if field in request.META:
+                if ',' in request.META[field]:
+                    parts = request.META[field].split(',')
+                    request.META[field] = parts[-1].strip()
